4ac436b52142206159c7fb925e6251c8e96c7e7b
Lollapalozar <1549120955@qq.com>
2022-05-22 23:03:30 +0800

    Site updated: 2022-05-22 23:03:29

--- js/word-Taoism.js
+++ js/word-Taoism.js
@@ -2,7 +2,7 @@
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array("临", "斗", "者", "皆", "阵", "列", "前", "行");
+        var a = new Array("临", "兵","斗", "者", "皆", "阵", "列", "前", "行");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
