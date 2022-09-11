f6e99dcd1f64ac37fae33110791466dcf68fa3ae
朱恒锋 <zhuhengfeng@MacBook-Air.local>
2022-09-11 15:51:19 +0800

    Site updated: 2022-09-11 15:51:19

---
+++ dist/music 2.js
@@ -0,0 +1,15 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    autoplay: false,
+    loop: 'all',
+    volume: 0.7,
+    listFolded: true,
+    listMaxHeight: 60,
+    fixed: true,
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: 'url.mp3',
+        cover: 'cover.jpg',
+    }]
+});
